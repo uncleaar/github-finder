5df1b1e5e1ed758dbd5385f2d52574bb68f42617
harld-x <danilyudin723@gmail.com>
2020-09-13 20:12:36 +0700

    changed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, {Component} from 'react';
 import Navbar from "./components/layout/Navbar";
 import './App.css'
 import Users from "./components/users/Users";
-
+import axios from 'axios'
 class App extends Component{
-  render() {
+    state = {
+        users: [],
+        loading: false
+    }
+
+    async componentDidMount() {
+        this.setState({
+            loading: true
+        })
+        const res = await axios.get('https://api.github.com/users')
+        this.setState({users: res.data, loading: false})
+    }
+
+    render() {
     return(
       <div className='App'>
         <Navbar/>
         <div className='container'>
-            <Users/>
+            <Users
+                loading={this.state.loading}
+                users={this.state.users}/>
         </div>
       </div>
     )
