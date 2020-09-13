472cdf0ea9ae448349ede3558decaea281c9a1ec
harld-x <danilyudin723@gmail.com>
2020-09-13 19:56:29 +0700

    hardcode for test users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-    render() {
-        const {login, avatar_url, html_url} = this.props.user
-
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
         return (
             <div className="card text-center">
                <img src={avatar_url}
@@ -20,7 +18,10 @@ class UserItem extends Component {
                 </div>
             </div>
         );
-    }
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem;
